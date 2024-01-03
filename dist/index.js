800549462d83d50567f8afd04a27df6f1d43b064
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-03 22:47:59 +0400

    c

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,9 @@
 "use strict";
 let mySize = 2;
 console.log(mySize);
+let emplo = {
+    id: 1,
+    name: "ddd",
+    reti: (date) => console.log(date),
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-function cal(income: number): number {
-  return 0
+const enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+
+let mySize: Size = Size.Medium
+
+console.log(mySize)
+
+type Maa = {
+  readonly id: number
+  name: string
+  reti: (date: Date) => void
+}
+
+let emplo: Maa = {
+  id: 1,
+  name: "ddd",
+  reti: (date: Date) => console.log(date),
 }
